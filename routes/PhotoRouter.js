2ad2374e283bc8dff317a08c9d5720c789d4dae6
CodeSandbox <support@codesandbox.io>
2025-11-22 04:20:06 +0000

    Sandbox snapshot commit

--- routes/PhotoRouter.js
+++ routes/PhotoRouter.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const Photo = require("../db/photoModel");
+const User = require("../db/userModel");
 const router = express.Router();
 
-router.post("/", async (request, response) => {
-  
-});
+router.post("/", async (request, response) => {});
 
-router.get("/", async (request, response) => {
-  
+router.get("/photosOfUser/:id", async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "Not found!" });
+    }
+    const photos = await Photo.find({
+      user_id: req.params.id,
+    }).select("_id user_id file_name date_time comments");
+    return res.json(photos);
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({ message: "Error!" });
+  }
 });
 
 module.exports = router;
--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -2,12 +2,24 @@ const express = require("express");
 const User = require("../db/userModel");
 const router = express.Router();
 
-router.post("/", async (request, response) => {
-  
+router.get("/list", async (request, response) => {
+  const data = await User.find({}, "_id first_name last_name");
+  return response.json(data);
 });
 
-router.get("/", async (request, response) => {
-  
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id).select(
+      "_id first_name last_name location description occupation"
+    );
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
+    }
+    return res.json(user);
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({ message: "Invalid user id" });
+  }
 });
 
 module.exports = router;
