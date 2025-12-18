5c9ddf5d068687da4720e812a92d97b178df0a0c
CodeSandbox <support@codesandbox.io>
2025-12-18 11:24:26 +0000

    Sandbox snapshot commit

--- db/userModel.js
+++ db/userModel.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
+  login_name: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
   first_name: { type: String },
   last_name: { type: String },
   location: { type: String },
--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const session = require("express-session");
 const dbConnect = require("./db/dbConnect");
 const UserRouter = require("./routes/UserRouter");
 const PhotoRouter = require("./routes/PhotoRouter");
 const CommentRouter = require("./routes/CommentRouter");
+const AdminRouter = require("./routes/AdminRouter");
 
 dbConnect();
 
-app.use(cors());
+app.use(cors({ credentials: true, origin: true }));
 app.use(express.json());
+app.use(
+  session({
+    secret: "photo-sharing-secret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
 app.use("/api/user", UserRouter);
 app.use("/api/photo", PhotoRouter);
+app.use("/api/admin", AdminRouter);
 
 app.get("/", (request, response) => {
   response.send({ message: "Hello from photo-sharing app API!" });
