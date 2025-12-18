182865e6f452a32fe1719bdde26a8087dd329986
CodeSandbox <support@codesandbox.io>
2025-12-18 14:17:11 +0000

    Sandbox snapshot commit

--- routes/AdminRouter.js
+++ routes/AdminRouter.js
@@ -2,3 +2,39 @@ const express = require("express");
 const bcrypt = require("bcrypt");
 const User = require("../db/userModel");
 const router = express.Router();
+
+// Login
+router.post("/login", async (req, res) => {
+  const { login_name, password } = req.body;
+  if (!login_name || !password) {
+    res.status(400).send("Username or password invalid");
+  }
+
+  const user = await User.findOne({ login_name });
+  if (!user) {
+    res.status(400).send("Wrong user");
+  }
+  const valid = await bcrypt.compare(password, user.password);
+  if (!valid) {
+    res.status(400).send("Wrong password");
+  }
+  req.session.user = {
+    _id: user._id,
+    first_name: user.first_name,
+  };
+  res.json({
+    _id: user._id,
+    first_name: user.first_name,
+  });
+});
+
+// Logout
+router.post("/logout", async (req, res) => {
+  if (!req.session.user) {
+    res.status(400).send("Invalid");
+  }
+  req.session.destroy();
+  res.sendStatus(200);
+});
+
+module.exports = router();
